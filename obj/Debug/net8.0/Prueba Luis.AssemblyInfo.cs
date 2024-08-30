12eaa57e4e4b12e71ccd4eb507dd46c3981a6cfe
Luis-Alberto2410 <ing.luis_juarez_santos@outlook.com>
2024-08-29 22:18:29 -0600

    espacio
    prueba espacio

--- obj/Debug/net8.0/Prueba Luis.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prueba Luis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Luis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1d155c2b3f79a634cf5727c343735388ffade1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Luis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Luis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
